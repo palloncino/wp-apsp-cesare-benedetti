595c5ebf10e1387e79c1d4ecc3b3bbae3a20b699
palloncino <antonio.guiotto.dev@gmail.com>
2024-09-18 19:57:06 +0200

    progress

--- plugins/services-rsa/index.php
+++ plugins/services-rsa/index.php
@@ -20,15 +20,15 @@ function services_rsa_render() {
         <h4 class="services-rsa__title">R.S.A.</h4>
         <nav class="services-rsa__nav layout_ver place_inside">
             <ul class="services-rsa__list level_1">
-                <li class="services-rsa__item level_1"><a class="services-rsa__link level_1" target="" href="https://www.apsp-cesarebenedetti.it/r-s-a-/32-3/">R.S.A.</a></li>
-                <li class="services-rsa__item level_1"><a class="services-rsa__link level_1" target="" href="https://www.apsp-cesarebenedetti.it/casa-di-soggiorno/32-32/">Casa di soggiorno</a></li>
-                <li class="services-rsa__item level_1"><a class="services-rsa__link level_1" target="" href="https://www.apsp-cesarebenedetti.it/centro-diurno/32-10/">Centro diurno</a></li>
-                <li class="services-rsa__item level_1"><a class="services-rsa__link level_1" target="" href="https://www.apsp-cesarebenedetti.it/hospice/32-11/">Hospice</a></li>
-                <li class="services-rsa__item level_1"><a class="services-rsa__link level_1" target="" href="https://www.apsp-cesarebenedetti.it/fisioterapia-per-esterni/32-12/">Fisioterapia per esterni</a></li>
-                <li class="services-rsa__item level_1"><a class="services-rsa__link level_1" target="" href="https://www.apsp-cesarebenedetti.it/infermieri-e-medici-specialistici/32-14/">Infermieri e medici specialistici</a></li>
-                <li class="services-rsa__item level_1"><a class="services-rsa__link level_1" target="" href="https://www.apsp-cesarebenedetti.it/assistenza-domiciliare-specializzata/32-108/">Assistenza domiciliare specializzata</a></li>
-                <li class="services-rsa__item level_1"><a class="services-rsa__link level_1" target="" href="https://www.apsp-cesarebenedetti.it/modulistica/32-19/">Modulistica</a></li>
-                <li class="services-rsa__item level_1"><a class="services-rsa__link level_1" target="" href="https://www.apsp-cesarebenedetti.it/tariffe-e-servizi/32-18/">Tariffe e servizi</a></li>
+                <li class="services-rsa__item level_1"><a class="services-rsa__link level_1" target="" href="/i-nostri-servizi/">R.S.A.</a></li>
+                <li class="services-rsa__item level_1"><a class="services-rsa__link level_1" target="" href="/casa-di-soggiorno/">Casa di soggiorno</a></li>
+                <li class="services-rsa__item level_1"><a class="services-rsa__link level_1" target="" href="/centro-diurno/">Centro diurno</a></li>
+                <li class="services-rsa__item level_1"><a class="services-rsa__link level_1" target="" href="/hospice/">Hospice</a></li>
+                <li class="services-rsa__item level_1"><a class="services-rsa__link level_1" target="" href="/fisioterapia-per-esterni/">Fisioterapia per esterni</a></li>
+                <li class="services-rsa__item level_1"><a class="services-rsa__link level_1" target="" href="/infermieri-e-medici-specialistici/">Infermieri e medici specialistici</a></li>
+                <li class="services-rsa__item level_1"><a class="services-rsa__link level_1" target="" href="/assistenza-domiciliare-specializzata/">Assistenza domiciliare specializzata</a></li>
+                <li class="services-rsa__item level_1"><a class="services-rsa__link level_1" target="" href="/modulistica/">Modulistica</a></li>
+                <li class="services-rsa__item level_1"><a class="services-rsa__link level_1" target="" href="/tariffe-e-servizi/">Tariffe e servizi</a></li>
             </ul>
         </nav>
     </div>
@@ -41,4 +41,6 @@ function services_rsa_shortcode() {
     return services_rsa_render();
 }
 add_shortcode('services-rsa', 'services_rsa_shortcode');
+
+
 
