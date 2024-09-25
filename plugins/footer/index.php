cd8da1c18e81dcc9139296a89273e7a998d7da69
palloncino <antonio.guiotto.dev@gmail.com>
2024-09-25 21:50:05 +0200

    progress

--- plugins/footer/index.php
+++ plugins/footer/index.php
@@ -2,13 +2,14 @@
 /*
 Plugin Name: Custom Footer Plugin
 Description: Automatically adds a custom footer to every page for the APSP Cesare Benedetti website.
-Version: 1.1
+Version: 1.2
 Author: Antonio Guiotto
 */
 
 // Function to enqueue the CSS for the footer
 function custom_footer_plugin_enqueue_styles() {
     wp_enqueue_style('custom-footer-plugin-css', plugins_url('footer-styles.css', __FILE__));
+    wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css');
 }
 add_action('wp_enqueue_scripts', 'custom_footer_plugin_enqueue_styles');
 
@@ -21,15 +22,19 @@ function custom_footer_plugin_render() {
                 <div class="footer-cols">
                     <div class="footer-col">
                         <div class="footer-widget">
-                            <h4><a style="color: rgb(178, 149, 126);" href="https://www.apsp-cesarebenedetti.it/le-novita/52-0-1/">NOVITÀ</a></h4>
+                            <h4><a href="https://apspcesarebenedetti.chebellagiornata.it/le-novita/">NOVITÀ</a></h4>
                             <div class="footer-bloglist">
                                 <div class="footer-bloglist-items">
+                                <div class="footer-bloglist-item">
+                                        <a href="#">Avviso di pubblica selezione per la figura di Assistente Amministrativo Categoria C – livello...</a>
+                                        <span class="footer-bloglist-date">21 Settembre, 2024</span>
+                                    </div>
                                     <div class="footer-bloglist-item">
-                                        <a href="https://www.apsp-cesarebenedetti.it/8-corso-per-volontari-in-cure-palliative-e-serate-per-la-comunit-/53-84/" class="footer-bloglist-link">8° corso per volontari in cure palliative e serate per la comunità</a>
+                                        <a href="#">8° corso per volontari in cure palliative e serate per la comunità</a>
                                         <span class="footer-bloglist-date">18 Luglio, 2024</span>
                                     </div>
                                     <div class="footer-bloglist-item">
-                                        <a href="https://www.apsp-cesarebenedetti.it/concorso-pubblico-per-esami-per-la-copertura-di-n-6-sei-posti-vacanti-a-tempo-pieno-36h-sett-nella-figura-professionale-di-infermiere--cat-c-evoluto---prima-posizione-retributiva/53-87/" class="footer-bloglist-link">Pubblicazione bando concorso per n. 6 posti di “Infermiere” </a>
+                                        <a href="#">Pubblicazione bando concorso per n. 6 posti di “Infermiere”</a>
                                         <span class="footer-bloglist-date">30 Luglio, 2024</span>
                                     </div>
                                 </div>
@@ -42,48 +47,47 @@ function custom_footer_plugin_render() {
                             <p>L’Azienda Pubblica di Servizi alla Persona Cesare Benedetti è un Ente di diritto pubblico senza finalità di lucro e convenzionato con l’Azienda Provinciale per i Servizi Sanitari di Trento.</p>
                             <p>Volge a garantire una qualità di vita il più possibile elevata al cliente anziano, autonomo o meno, considerandone i peculiari bisogni psichici, fisici e sociali, attraverso un'assistenza qualificata e continua, in stretta collaborazione con la famiglia.</p>
                         </div>
-                        <br><br>
-                        <a href="https://www.trentinofamiglia.it/Certificazioni-e-reti/Family-Audit" target="_blank" style="text-decoration: none; color: rgb(233, 228, 219);">
-                            <img class="footer-logo-img" src="https://www.apsp-cesarebenedetti.it/skin/cesarebenedetti_riev/images/jpg/family_audit/" alt="" style="margin-top:30px; width: 60%; height:auto;">
-                        </a>
+                        <div class="footer-image-container">
+                            <a href="https://www.trentinofamiglia.it/Certificazioni-e-reti/Family-Audit" target="_blank">
+                                <img class="footer-logo-img" src="http://apspcesarebenedetti.chebellagiornata.it/wp-content/uploads/2024/09/asd.png" alt="Family Audit">
+                            </a>
+                        </div>
                     </div>
                     <div class="footer-col">
                         <div class="footer-widget">
                             <h4>CONTATTI</h4>
                             <div class="footer-contacts">
-                                <div class="footer-contacts-inner">
-                                    <div class="footer-contacts-list">
-                                        <div class="footer-contacts-item">
-                                            <i class="fa fa-map-marker"></i>
-                                            <span class="footer-contacts-item-value">Via Del Garda 62, 38065 Mori, Trento (TN)</span>
-                                        </div>
-                                        <div class="footer-contacts-item">
-                                            <i class="fa fa-phone"></i>
-                                            <span class="footer-contacts-item-value">+39 0464 075001</span>
-                                        </div>
-                                        <div class="footer-contacts-item">
-                                            <i class="fa fa-fax"></i>
-                                            <span class="footer-contacts-item-value">+39 0464 071219</span>
-                                        </div>
-                                        <div class="footer-contacts-item">
-                                            <i class="fa fa-envelope-o"></i>
-                                            <span class="footer-contacts-item-value"><a href="mailto:segreteria@apsp-cesarebenedetti.it">segreteria@apsp-cesarebenedetti.it</a></span>
-                                        </div>
-                                        <div class="footer-contacts-item">
-                                            <i class="fa fa-envelope"></i>
-                                            <span class="footer-contacts-item-value"><a href="mailto:amministrazione@pec.apsp-cesarebenedetti.it">amministrazione@pec.apsp-cesarebenedetti.it</a></span>
-                                        </div>
-                                        <center>
-                                            <a href="https://mypay.provincia.tn.it/pa/home.html " target="_blank" style="text-decoration: none; color: rgb(233, 228, 219);">
-                                                <img class="footer-logo-img" src="https://www.apsp-cesarebenedetti.it/skin/cesarebenedetti_riev/images/png/logo_pagopa_mypay/" alt="Pago PA - MyPay" style="margin-top:30px; width: 60%; height:auto;">
-                                            </a>
-                                        </center>
-                                        <center>
-                                            <a href="https://www.trentinofamiglia.it/News-eventi/News/Distretto-Family-Audit-Alta-Valsugana-welfare-aziendale-in-rete" target="_blank" style="text-decoration: none; color: rgb(233, 228, 219);">
-                                                <img class="footer-logo-img" src="https://www.apsp-cesarebenedetti.it/skin/cesarebenedetti_riev/images/png/distretto_family/" alt="" style="margin-top:30px; width: 60%; height:auto;">
-                                            </a>
-                                        </center>
+                                <div class="footer-contacts-list">
+                                    <div class="footer-contacts-item">
+                                        <i class="fas fa-map-marker-alt"></i>
+                                        <span class="footer-contacts-item-value">Via Del Garda 62, 38065 Mori, Trento (TN)</span>
                                     </div>
+                                    <div class="footer-contacts-item">
+                                        <i class="fas fa-phone"></i>
+                                        <span class="footer-contacts-item-value">+39 0464 075001</span>
+                                    </div>
+                                    <div class="footer-contacts-item">
+                                        <i class="fas fa-fax"></i>
+                                        <span class="footer-contacts-item-value">+39 0464 071219</span>
+                                    </div>
+                                    <div class="footer-contacts-item">
+                                        <i class="fas fa-envelope"></i>
+                                        <span class="footer-contacts-item-value"><a href="mailto:segreteria@apsp-cesarebenedetti.it">segreteria@apsp-cesarebenedetti.it</a></span>
+                                    </div>
+                                    <div class="footer-contacts-item">
+                                        <i class="fas fa-envelope-open"></i>
+                                        <span class="footer-contacts-item-value"><a href="mailto:amministrazione@pec.apsp-cesarebenedetti.it">amministrazione@pec.apsp-cesarebenedetti.it</a></span>
+                                    </div>
+                                </div>
+                                <div class="footer-image-container">
+                                    <a href="https://mypay.provincia.tn.it/pa/home.html" target="_blank">
+                                        <img class="footer-logo-img" src="http://apspcesarebenedetti.chebellagiornata.it/wp-content/uploads/2024/09/pagopa.png" alt="Pago PA - MyPay">
+                                    </a>
+                                </div>
+                                <div class="footer-image-container">
+                                    <a href="https://www.trentinofamiglia.it/News-eventi/News/Distretto-Family-Audit-Alta-Valsugana-welfare-aziendale-in-rete" target="_blank">
+                                        <img class="footer-logo-img" src="http://apspcesarebenedetti.chebellagiornata.it/wp-content/uploads/2024/09/distretto-family-1.png" alt="Distretto Family">
+                                    </a>
                                 </div>
                             </div>
                         </div>
@@ -92,16 +96,18 @@ function custom_footer_plugin_render() {
             </div>
             <div class="footer-bottom">
                 <div class="footer-bottom-inner">
-                    <div class="footer-copyright">© 2015 A.P.S.P. Cesare Benedetti | Partita IVA: 00323360222 | <a href="https://form.agid.gov.it/view/7707f7d2-41ac-4b89-b5de-58e8b3cd059d">Accessibilità Agid</a></div>
+                    <div class="footer-copyright">
+                        © 2024 A.P.S.P. Cesare Benedetti | Partita IVA: 00323360222 | <a href="https://form.agid.gov.it/view/7707f7d2-41ac-4b89-b5de-58e8b3cd059d">Accessibilità Agid</a>
+                    </div>
                     <nav class="footer-nav">
                         <ul class="footer-nav-list">
                             <li class="footer-nav-item"><a class="footer-nav-link" href="#">Home</a></li>
-                            <li class="footer-nav-item"><a class="footer-nav-link" href="https://www.apsp-cesarebenedetti.it/chi-siamo/32-1/">Chi siamo</a></li>
-                            <li class="footer-nav-item"><a class="footer-nav-link" href="https://www.apsp-cesarebenedetti.it/dove-siamo/32-2/">Dove Siamo</a></li>
-                            <li class="footer-nav-item"><a class="footer-nav-link" href="https://www.apsp-cesarebenedetti.it/servizi/32-3/">Servizi</a></li>
-                            <li class="footer-nav-item"><a class="footer-nav-link" href="https://www.apsp-cesarebenedetti.it/news/52-0-1/">News</a></li>
-                            <li class="footer-nav-item"><a class="footer-nav-link" href="https://www.apsp-cesarebenedetti.it/note-legali/32-23/">Note legali</a></li>
-                            <li class="footer-nav-item"><a class="footer-nav-link" href="https://www.apsp-cesarebenedetti.it/privacy/32-118/">Privacy</a></li>
+                            <li class="footer-nav-item"><a class="footer-nav-link" href="https://apspcesarebenedetti.chebellagiornata.it/chi-siamo/">Chi siamo</a></li>
+                            <li class="footer-nav-item"><a class="footer-nav-link" href="https://apspcesarebenedetti.chebellagiornata.it/dove-siamo/">Dove Siamo</a></li>
+                            <li class="footer-nav-item"><a class="footer-nav-link" href="https://apspcesarebenedetti.chebellagiornata.it/servizi/">Servizi</a></li>
+                            <li class="footer-nav-item"><a class="footer-nav-link" href="https://apspcesarebenedetti.chebellagiornata.it/news/">News</a></li>
+                            <li class="footer-nav-item"><a class="footer-nav-link" href="https://apspcesarebenedetti.chebellagiornata.it/note-legali/">Note legali</a></li>
+                            <li class="footer-nav-item"><a class="footer-nav-link" href="https://apspcesarebenedetti.chebellagiornata.it/privacy/">Privacy</a></li>
                         </ul>
                     </nav>
                 </div>
@@ -110,6 +116,5 @@ function custom_footer_plugin_render() {
     </div>
     <?php
 }
-
-// Hook the footer render function into the wp_footer action
 add_action('wp_footer', 'custom_footer_plugin_render', 20);
+?>
